cec76c65a6c92b4a1a43b8ea32adfc0522017be6
Victor Sabry <victorsabry@hotmail.com>
2022-06-14 09:26:08 +0200

    Add project files.

---
+++ BadetidsSystemet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BadetidsSystemet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
